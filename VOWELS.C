221a76fe42f23f1ae55f8414964cc535fcb694ac
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-01-21 16:29:23 +0530

    VOWEL.C
    TO FIND THE GIVEN CHARACTER IS VOWELS OR CONSONANT

---
+++ VOWELS.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char [i]=50;
+i=(a,e,i,o,u,A,E,I,O,U);
+printf("\nEnter the character %c,",i)
+scanf("%c",&i);
+if(i)
+{
+printf("\nThe given character is vowel",i);
+}
+else(!=i)
+{
+printf("\nThe given number is consonant",i);
+}
+return 0;
+}
