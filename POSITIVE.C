9d4715d5bce462ae0f636ddf15f1aa712f16ea67
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-01-21 16:10:44 +0530

    POSITIVE.C
    TO FIND THE GIVEN NUMBER IS POSITIVE OR NEGATIVE OR ZERO

---
+++ POSITIVE.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("\nEnter the number %d",a)
+scanf("%d",&a);
+if(a>0)
+{
+printf("\nThe given number is positive",a);
+}
+if else(a<0)
+{
+printf(\nThe given number is negative",a);
+}
+else(a=0)
+{
+printf(\nThe given number is zero",a);
+}
+return o;
+}
