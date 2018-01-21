7597c3546eab31cfe742129d622ee3f9b1631004
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-01-21 15:53:12 +0530

    ODDEVEN.C
    TO FIND THE GIVEN NUMBER IS ODD OR EVEN

---
+++ ODDEVEN.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("\nEnter the number %d",a);
+scanf("%d",a);
+if(a%2==0)
+{
+scanf("%d",a);
+printf("\nThe given number is even");
+}
+else
+{
+printf("\nThe given number is odd");
+}
+return 0;
+}
