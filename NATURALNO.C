6894d2f788fdc27bb6a6624fb830e42d5f8d839e
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-01-22 15:57:33 +0530

    NATURALNO.C
    TO FIND THE PROGRAM TO CALCULATE THE SUM OF THE NATURAL NUMBERS UPTO N NUMBERS

---
+++ NATURALNO.C
@@ -0,0 +1,14 @@
+#include<stdio.h
+int main()
+{
+int i,n,sum=0;
+scanf("%d",&n);
+printf("\nEnter the natural numbers",n);;
+sum=+n;
+{
+printf("\nThe sum of the Natural numbers is:",sum);
+return 0;
+}
+}
+
+
