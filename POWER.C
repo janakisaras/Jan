ee4e2a528962d4fcf8cf05f1693188e59d219570
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-01-31 16:41:43 +0530

    POWER.C
    TO FIND  CALCULATE THE  POWER  OF GIVEN NUMBER

---
+++ POWER.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("\nEnter the number");
+scanf("%d",&a);
+printf("\nEnter the power");
+scanf("%d",&b);
+c=a^b;
+printf("\nThe calculation of number is %d",&c);
+return 0;
+}
