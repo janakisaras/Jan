6ee5710ab0fd9c39e8142a473e650d8d0bc1b4fc
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-01-31 16:55:18 +0530

    EVEN.C
    TO FIND THE EVEN NUMBERS BETWEEN TWO NUMBERS

---
+++ EVEN.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i,a,b;
+printf("\nEnter the number ");
+scanf("%d",&a);
+printf("\nEnter the number ");
+scanf("%d",&b);
+for(i=0;i<=b;i++)
+{
+if(i%2==0)
+{
+printf("\n%d",i);
+}
+return 0;
+}
