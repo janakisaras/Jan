f9d107efda778035c97654c2ad927fdd3c8b3b2d
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-02-02 15:48:23 +0530

    INTARRAY.C
    TO FIND THE  PROGRAM FOR SUM OF THE ARRAY OF INTEGER

---
+++ INTARRAY.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i,n,j,K,N[20];
+printf("\nEnter the intrger value:);
+scanf("%d",&K);
+for(i=0;i<=n;i++)
+{
+printf("\nEnter the array of intgers:");
+scanf("%d",&N);
+}
+j=K+N;
+printf("\nThe sum of the array of integer is %d",j);
+return 0;
+}
