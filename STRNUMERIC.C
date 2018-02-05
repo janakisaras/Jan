0a514e5a50bab4c439c1746cad88a17b04bebff1
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-02-05 16:56:44 +0530

    STRNUMERIC.C
    THE PROGRAM TO FIND THE VALIDATE  IF THE STRING IS NUMERIC

---
+++ STRNUMERIC.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int a,b;
+printf("\nEnter the string");
+scanf("%d",&a);
+if('a'=='0'||'a'=='1'||'a'=='2'||'a'=='3'||'a'=='4'||'a'=='5'||'a'=='6'||'a'=='7'||'a'=='8'||'a'=='9'||'a'=='0')
+{
+printf("\nThe given string is numeric");
+}
+else
+{
+printf("\nNo");
+}
+return 0;
+}
