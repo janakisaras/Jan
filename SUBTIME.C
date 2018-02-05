18adb62e14aa3a73a327be4e7254ade202150c32
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-02-05 16:35:57 +0530

    SUBTIME.C
    THE PROGRAM TO FIND THE SUBTRACTION OF THE GIVEN TIMES

---
+++ SUBTIME.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d,e;
+printf("\nEnter the hours and minutes");
+scanf("%d,%d",&a,&b);
+printf("\nEnter the hours and minutes");
+scanf("%d,%d",&c,&d);
+printf("\n%d:%d",a,b);
+printf("\n%d:%d",c,d);
+e=((a,b)-(c,d));
+printf("\n%d:%d",e);
+return 0;
+}
