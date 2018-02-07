9a42193925194114ab0221a9b2cb0e9c405bef0c
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-02-07 16:34:31 +0530

    MEDIAN.C
    THE PROGRAM TO FIND THE GIVEN NUMBER N AND AN ARRAY OF N INTEGER AND PRINT THE MEDIAN OF GIVEN NUMBER

---
+++ MEDIAN.C
@@ -0,0 +1,37 @@
+#include<stdio.h>
+ int main( )                                                     
+   {                                                           
+       int i,j,n;                                              
+       float median,a[N],t;                                    
+                                                               
+       printf("Enter the number of items\n");                  
+       scanf("%d", &n);                                        
+       printf("Input %d values \n",n);                         
+       for (i = 1; i <= n ; i++)                               
+       scanf("%f", &a[i]);                                 
+       for (i = 1 ; i <= n-1 ; i++)                            
+       { 
+       for (j = 1 ; j <= n-i ; j++)                         
+       {                                                    
+       if (a[j] <= a[j+1])                              
+            {
+             t = a[j];                                      
+             a[j] = a[j+1];                                 
+                a[j+1] = t;                                    
+              }                                                
+              elsecontinue ;                                     
+          }                                                    
+       } 
+        if ( n % 2 == 0)                                        
+          median = (a[n/2] + a[n/2+1])/2.0 ;                   
+       else                                                    
+          median = a[n/2 + 1];                                 
+                                                               
+    
+for (i = 1 ; i <= n ; i++)                              
+           printf("%f  ", a[i]);                               
+       printf("\n\n The given number Median is %f\n", median);                   
+                                                               
+   }    
+                                                       
+        
