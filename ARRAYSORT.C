bbed6cfbe8b3698a5cef59bba2804392ff0ea295
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-02-07 16:19:28 +0530

    ARRAYSORT.C
    THE PROGRAM TO FIND THE GIVEN A NUMBER N AND AN ARRAY OF N INTEGER AND PRINT THE SOTED ARRAY

---
+++ ARRAYSORT.C
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define MAXSIZE 10
+ 
+void main()
+{
+    int array[MAXSIZE];
+    int i, j, integer, temp;
+ 
+    printf("Enter the value of integer \n");
+    scanf("%d", &integer);
+    printf("Enter the elements one by one \n");
+    for (i = 0; i < integer; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    printf("Input array is \n");
+    for (i = 0; i < num; i++)
+    {
+        printf("%d\n", array[i]);
+    }
+    for (i = 0; i < integer; i++)
+    {
+        for (j = 0; j < (integer - i - 1); j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    printf("Sorted array is...\n");
+    for (i = 0; i < integer; i++)
+    {
+        printf("%d\n", array[i]);
+    }
+}
