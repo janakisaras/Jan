6cd4b738918e7294929d583dd8bfcab75baa0068
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-02-12 16:12:03 +0530

    ARRAYINDEX.C
    THE PROGRAM TO PRINT THE ARRAY ELEMENTS ITS INDEX

---
+++ ARRAYINDEX.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i,n,b,c;
+int array[i];
+printf("\nEnter the number of elements");
+scanf("%d",&i);
+for(i=0;i<=n;i++)
+printf("\nThe array elements are:");
+printf("\nThe index of array elements are:");
+return 0;
+}
+
