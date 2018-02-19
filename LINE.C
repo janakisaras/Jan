bfa7cbf70a4e86f56804cf98b6ddb3600342a70e
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-02-19 16:11:42 +0530

    LINE.C
    THE PROGRAM TO COUNT THE CHAR,WORDS,SENTENCES IN THE GIVEN PARAGRAPH

---
+++ LINE.C
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() 
+{  
+int count = 0;  
+int words = 0;  
+int sentences = 0;  
+char ch;
+
+while ((ch = getch()) != '\n')
+{
+ printf("%c", ch);
+ while ((ch = getch()) != '.')
+{
+ printf("%c", ch);
+ while ((ch = getch()) != ' ')
+{
+ printf("%c", ch);
+ count++;
+}
+ printf("%c", ch);
+ words++;
+}
+ sentences++;
+}
+
+printf("The number of characters are %d", count);
+printf("\nThe number of words are %d", words);
+printf("\nThe number of sentences are %d", sentences);
+return 0;
+}
