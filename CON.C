9cfe2e2a6a668a17cb42b1cb24dacd0085cc2398
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-02-21 16:30:29 +0530

    con.c
    THE PROGRAM TO CONCATENATE TWO STRINGS WITHOUT USING STRCAT() FUNCTION

---
+++ CON.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+
+char s,j;
+printf("\nEnter the 1st string");
+scanf("%s",&s);
+printf("\nEnter the 2nd string");
+scanf("%s",&j);
+printf("\nThe concatenate of two string is %s%s",s,j);
+return 0;
+}
