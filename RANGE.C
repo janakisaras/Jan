5d88f1c33c9a2add8054f107bbfe851ce7a7b089
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-02-23 16:00:00 +0530

    RANGE.C
    THE PROGRAM TO CHECK WHETHER THE GIVEN NUMBER N IS IN THE RANGE OR NOT

---
+++ RANGE.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,N;
+	printf("\nEnter the range:");
+	scanf("%d%d",&i,&j);
+	for(i=0;i<j;i++)
+	printf("\nEnter the number :");
+	scanf("%d",&N);
+	if(N=i,j)
+	{
+		printf("\nYES");
+		
+	}
+	else
+	{
+		printf("\nNO");
+	}
+return 0;
+}
