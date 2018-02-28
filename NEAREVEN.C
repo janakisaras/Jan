5f90758b16c180c077f88c1aca7451659f05a84a
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-02-28 16:55:34 +0530

    NEAREVEN.C
    THE PROGRAM TO FIND THE GIVEN NUMBER OR THE NEAREST NUMBER IS EVEN

--- NEAREVEN.C
+++ NEAREVEN.C
@@ -5,16 +5,16 @@ int a;
 printf("\nEnter the number N is");
 scanf("%d",&a);
 a--;
-if(a%2)
+if(a%2=0)
 
 printf("\nThe given number's nearest number is even %d",a--);
 
 a++;
-elif(a%2)
+elif(a%2=0)
 
 printf("\nThe given number's nearest number is even %d",a++);
 
-else(a%2)
+else(a%2=0)
 
 printf("\nThe given number is equal to the even number %d");
 
