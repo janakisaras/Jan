5abc25d91fc9d656cd92ca890adea164b4438052
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-02-28 16:51:52 +0530

    EVENEVEN.C
    THE PROGRAM TO FIND THE SUM OF TWO NUMBER IS EVEN OR ODD

---
+++ EVENEVEN.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int N,M,J;
+printf("\nEnter the number ");
+scanf("%d",&N);
+printf("\nEnter the number");
+scanf("%d",&M);
+J=N+M;
+printf("\nThe sum of the two number is %d",J);
+if(J%2=0)
+{
+printf("\nThe number is the even");
+}
+else
+printf("\nThe given number is odd");
+return 0;
+}
