0a351ad4951a9540f8c02dbdb93b43a7e9f3163d
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-02-28 16:40:54 +0530

    NEAREVEN.C
    THE PROGRAM TO PRINT THE EVEN NUMBER NEAR THE GIVEN NUMBER OR THAT NUMBER

---
+++ NEAREVEN.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("\nEnter the number N is");
+scanf("%d",&a);
+a--;
+if(a%2)
+
+printf("\nThe given number's nearest number is even %d",a--);
+
+a++;
+elif(a%2)
+
+printf("\nThe given number's nearest number is even %d",a++);
+
+else(a%2)
+
+printf("\nThe given number is equal to the even number %d");
+
+return 0;
+}
