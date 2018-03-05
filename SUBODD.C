3712acdc0e90b1081d0e1df748d54396d20fc11f
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-03-05 16:43:33 +0530

    SUBODD
    THE PROGRAM TO FIND THE GIVEN NUMBER'S DIFFERENCE IS ODD OR EVEN

--- SUBODD.C
+++ SUBODD.C
@@ -2,14 +2,14 @@
 #include<conio.h>
 int main()
 {
-	int a,b,c;
+	int i,j,k;
 	clrscr();
 	printf("\nEnter the number1 and number2");
-	scanf("%d%d",&a,&b);
-	c=a-b;
-	if(c%2==0)
-	printf("\nThe sum of two number is even %d",c);
+	scanf("%d%d",&i,&j);
+	k=i-j;
+	if(k%2==0)
+	printf("\nThe sum of two number is even %d",k);
 	else
-	printf("\nThe sum of two number is odd %d",c);
+	printf("\nThe sum of two number is odd %d",k);
 	getch();
 }
