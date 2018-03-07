21987a45d4a3d6933771edabe7794f798a424ace
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-03-07 16:15:41 +0530

    COUNTCOUNT.C
    THE PROGRAM TO FIND THE NUMBER WORDS IN THE GIVEN SENTENCES

---
+++ COUNTCOUNT.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s[200];
+    int count = 0, j;
+ clrscr();
+    printf("enter the string\n");
+    scanf("%[^\n]s", s);
+    for (j = 0;s[j] != '\0';j++)
+    {
+        if (s[j] == ' ')
+            count++;    
+    }
+    printf("\nNumber of words in given string are: %d\n", count + 1);
+getch();
+	
+}
