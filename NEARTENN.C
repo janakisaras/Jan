502ece2cbcceeec04920f32330dbde04a9b90984
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-03-07 15:46:24 +0530

    NEARTENN.C
    THE PROGRAM TO FIND THE GIVEN NUMBER'SNEAREST NUMBER MULTIPLE OF TEN

---
+++ NEARTENN.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b;
+	clrscr();
+	printf("\nEnter the number");
+	scanf("%d",&a);
+	b=(((a+9)/10)*10);
+	printf("\nThe nearest number is %d",b);
+getch();
+}
+	
