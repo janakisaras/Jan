6eef2ba7af202daf405fc443588ab4205a74e09c
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-03-09 16:49:02 +0530

    NEARINT.C
    THE PROGRAM TO PRINT THE GIVEN DECIMAL NUMBER IS ROUND OFF TO  NEAREST INTEGER NUMBER

---
+++ NEARINT.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main()
+{
+    float n;
+    clrscr();
+
+    printf("Enter the decimal number");
+    scanf("%f",&n);
+
+    printf("The decimal number after rounded is  %5.0f",n);
+    getch();
+}
