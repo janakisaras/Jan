60cd16996ae49d409d13bc1a6cd07b22e33db428
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-03-09 16:00:09 +0530

    FIRST.C
    THE PROGRAM TO PRINT THE K NUMBER OF CHARACTER IN THE GIVEN STRING

---
+++ FIRST.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ char c[100];
+ int k;
+ clrscr();
+ printf("\nEnter the string");
+ scanf("%s",&c);
+ printf("\nEnter the number");
+ scanf("%d",&k);
+ k=strlen;
+ printf("%c",k);
+ getch();
+}
