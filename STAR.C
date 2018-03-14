4e5bbb3d4f1982ae72433e888e01ac404af1488a
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-03-14 15:49:54 +0530

    STAR.C
    THE PROGRAM TO PRINT * IN THE GIVEN STRING'S MIDDLE OF THE LETTER

---
+++ STAR.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char s[50];
+	int a;
+	clrscr();
+	printf("\nEnter the string ");
+	scanf("%s",&s);
+	a=s/2;
+	a=strlen();
+	printf("\nThe string is",a);
+	a='*';
+	printf("the word is ",s,a);
+	getch();
+	
+}
