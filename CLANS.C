5094a19b1668d6dc26262b3230f3126ba420f0aa
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-03-14 16:10:54 +0530

    CLANS.C
    THE PROGRAM TO FIND THE DIFFERENCE BETWEEN THE TWO CLANS

---
+++ CLANS.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b,c;
+	(c<=(2^32));
+            printf("\nNo.of. ninjas in kabali's clans and his opponent's clans");
+	scanf("%d%d",&a,&b);
+           	c=b-a;
+           	printf("%d",c);
+           	return 0;
+           	
+
+	
+	
+}
