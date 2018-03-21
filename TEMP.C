c54cfe0df6e93afa8e30c6f9c8b79b1056bb452c
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-03-21 16:41:44 +0530

    TEMP.C
    THE PROGRAM TO CONVERT THE TEMPERATURE CELSIUS INTO KELVIN

---
+++ TEMP.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int c,k;
+clrscr();
+printf("\nEnthe the temperature in celsius");
+scanf("%d",&c);
+k=273+c;
+printf("\nThe celsius is convert into kelvin is %d",k);
+getch();
+}
